3fa9fd551e45bd54fc3648830d933949039aa483
Volodymyr <49057003+vovashvets@users.noreply.github.com>
2022-08-24 15:01:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 var items = document.getElementsByClassName("js-delete-branch");
 for (let i=0; i<=items.length; i++) {
     setTimeout(() => {
+	// Click on "Delete Git Branch"
     	items[i].click()
-
-		setTimeout(function(){
-	    	document.getElementById("confirm-text").value = 'DELETE'
-	    	document.getElementById("confirmation-dialog-confirm").disabled = false
-		}, 500)
-
-    	setTimeout(function(){
-			document.getElementById("confirmation-dialog-confirm").click()
-		}, 500)
+	// Enter validation value in the text field and enable submit btn
+	setTimeout(function(){
+		document.getElementById("confirm-text").value = 'DELETE'
+		document.getElementById("confirmation-dialog-confirm").disabled = false
+	}, 500)
+	// Click on sumbit btn
+	setTimeout(function(){
+		document.getElementById("confirmation-dialog-confirm").click()
+	}, 500)
     }, 1500 * i);
 }
